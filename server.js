8fbd57233f6b41332406aef068bbeb23031eed2b
Ilyas Assainov <iliyas.assainov@gmail.com>
2018-11-15 13:59:30 +0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 //  telling hbs we are going to use partials in its templates
@@ -64,6 +65,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up and running');
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
 });
